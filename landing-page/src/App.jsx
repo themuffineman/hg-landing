4d9211e1a48ceadb717e0c995196526020a6277f
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-09-21 10:56:43 +0000

    Init commit

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,32 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import './App.css'
+import Navbar from "./components/Navbar";
+import { About, Contact, Home, Projects } from "./pages";
+function App() {
+
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route
+            path='/*'
+            element={
+              <>
+                <Routes>
+                  <Route path='/about' element={<About />} />
+                  <Route path='/projects' element={<Projects />} />
+                  <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    </main>
+  )
+}
+
+export default App
---
+++ landing-page/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+   <div>
+     hello world
+   </div>
+  );
+};
+
+export default Navbar;
