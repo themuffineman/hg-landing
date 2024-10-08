3810663700170b7626f5d7448dd57c86fb87c985
Petrus Sheya <sheyastudios@gmail.com>
2024-10-08 18:19:00 +0000

    Edits 1

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -11,42 +11,39 @@ import { Home } from "./pages";
 
 const App = () => {
   return (
-      <main className=" w-full h-screen main-body">
-        <div className=" h-full] absolute inset-0 -z-10 w-full">
-          <Canvas className="h-full w-full"
-              camera={{
-                position: [10, 10, 10],
-                fov: 200,
-                aspect: window.innerWidth / window.innerHeight,
-                near: 0.1,
-                far: 1000,
-              }}
-              style={{ width: "100%", height: "500vh" }}
-          >
-            <directionalLight position={[1, 1, 1]} intensity={2} />
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 5, 10]} intensity={2} />
-            <spotLight
-              position={[0, 50, 10]}
-              angle={0.15}
-              penumbra={1}
-              intensity={2}
-            />
-            
-            <hemisphereLight
-              skyColor="#b1e1ff"
-              groundColor="#000000"
-              intensity={1}
-            />
-            <Sky />
-          </Canvas>
-        </div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </main>
+    <ReactLenis root>
+        <main className=" w-full h-screen main-body overflow-y-hidden">
+          <div className=" h-full absolute inset-0 -z-10 w-full">
+            <Canvas 
+                camera={{
+                  position: [0, 0, 0],
+                  fov: 50,
+                  aspect: window.innerWidth / window.innerHeight,
+                  near: 0.1,
+                  far: 1000,
+                }}
+                style={{ width: "100%", height: "100vh" }}
+            >
+              <directionalLight position={[1, 1, 1]} intensity={2} />
+              <ambientLight intensity={0.5} />
+              <pointLight position={[10, 5, 10]} intensity={2} />
+              <spotLight
+                position={[0, 50, 10]}
+                angle={0.15}
+                penumbra={1}
+                intensity={2}
+              />
+              <hemisphereLight
+                skyColor="#b1e1ff"
+                groundColor="#000000"
+                intensity={1}
+              />
+              <Sky />
+            </Canvas>
+          </div>
+          <Home />
+        </main>
+    </ReactLenis>
   );
 };
 
