c0d643fc006123fe8a176e05e56ae19f12a00a0f
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-11-26 19:07:27 +0200

    Update copy.js Updated copy text

--- landing-page/websiteCopy/copy.js
+++ landing-page/websiteCopy/copy.js
@@ -1,7 +1,7 @@
 export const copy = {
     heroDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque neque justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     howItWorks: {
-        step1: "How it works 1qwhrjhk",
+        step1: "How it works 1. Uplaod image",
         step2: "How it works 2",
         step3: "How it works 3",
         step4: "How it works 4"
