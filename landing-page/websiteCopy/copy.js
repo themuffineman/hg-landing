a5d5f6e363f9d9d25a4a5b0fa219e4ba38bcde50
Yourethehero <121990741+Yourethehero@users.noreply.github.com>
2024-12-16 17:47:58 -0500

    Update copy.js
    ADDED SOME CHANGES TO THE LANDING PAGE TEXT

--- landing-page/websiteCopy/copy.js
+++ landing-page/websiteCopy/copy.js
@@ -1,10 +1,11 @@
 export const copy = {
-    heroDescription: "Lorem ipsum dolor sit amet  , consectetur adipiscing elit. Quisque neque justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    heroDescription: "No more boring gifts that lack the personal touch of your loved ones and a pinch of humour. Hideous Gifts brings the best of gifts filled with humour and trolls for your friend and family
+        while bringing good habits and ambition for your children",
     howItWorks: {
-        step1: "How it works 1. Adding anything",
-        step2: "How it works 2",
-        step3: "How it works 3",
-        step4: "How it works 4"
+        step1: "First, choose the habit (for kids) or style (for adults) you want",
+        step2: "Next, upload your loved one's image and hit 'Craft My Art' to create your art. Hit again if you want to see another art if available",
+        step3: "Then, choose what product you want your customized art on to see what it looks and feels like with our live mock ups",
+        step4: "Once everything is confirmed simply pay and order"
     }
 
 }
