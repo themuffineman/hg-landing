4d01392f1759461ff244329164b65184bbf43be5
themuffineman <petrusheya@gmail.com>
2024-12-16 10:44:51 +0200

    images

--- landing-page/cardImages/cardImages.js
+++ landing-page/cardImages/cardImages.js
@@ -1,6 +1,1 @@
-export const images = [
-  "https://images.pexels.com/photos/29674315/pexels-photo-29674315/free-photo-of-relaxed-woman-in-vibrant-lavender-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/29674315/pexels-photo-29674315/free-photo-of-relaxed-woman-in-vibrant-lavender-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/29674315/pexels-photo-29674315/free-photo-of-relaxed-woman-in-vibrant-lavender-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/18884059/pexels-photo-18884059/free-photo-of-woman-lying-on-bed-with-laptop-and-candle.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-];
+export const images = ["how-to-1", "how-to-2", "how-to-3", "how-to-4"];
