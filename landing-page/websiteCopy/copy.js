1454137b158c95b49260e6dc007f659e5e647bd3
Petrus <131905449+themuffineman@users.noreply.github.com>
2024-12-17 01:25:38 +0200

    Update copy.js typo

--- landing-page/websiteCopy/copy.js
+++ landing-page/websiteCopy/copy.js
@@ -1,6 +1,5 @@
 export const copy = {
-    heroDescription: "No more boring gifts that lack the personal touch of your loved ones and a pinch of humour. Hideous Gifts brings the best of gifts filled with humour and trolls for your friend and family
-        while bringing good habits and ambition for your children",
+    heroDescription: "No more boring gifts that lack the personal touch of your loved ones and a pinch of humour. Hideous Gifts brings the best of gifts filled with humour and trolls for your friend and family while bringing good habits and ambition for your children",
     howItWorks: {
         step1: "First, choose the habit (for kids) or style (for adults) you want",
         step2: "Next, upload your loved one's image and hit 'Craft My Art' to create your art. Hit again if you want to see another art if available",
